9e41eae42e9fff6f4baf8799c4b0f1add19172bf
beewhoo <bibeksinghthapa@gmail.com>
2018-06-20 12:03:08 -0400

    created products, seeded database

--- yasuni/db/seeds.rb
+++ yasuni/db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(
+  name: 'Cellphone Case iPhone',
+  price_in_cents: 995,
+  description:'Case for iPhone'
+)
+
+
+Product.create(
+  name: 'Cellphone Case Galaxy',
+  price_in_cents: 805,
+  description:'Case for Galaxy'
+)
+
+Product.create(
+  name: 'Cellphone Case Pixel',
+  price_in_cents: 1005,
+  description:'Case for Pixel'
+)
