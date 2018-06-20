227dcc7531ee317a8de80e8402004486d0e19f8e
beewhoo <bibeksinghthapa@gmail.com>
2018-06-20 15:00:26 -0400

    completed product controller, general layout on application, stylesheets, generated a stellar logo using online vector based design software - p.s didnt pay for the logo - #screenshotthieves #longlivestevejobs -bravo out

--- yasuni/app/controllers/products_controller.rb
+++ yasuni/app/controllers/products_controller.rb
@@ -1,47 +1,69 @@
 class ProductsController < ApplicationController
 
-  def index
-    @products = Product.all 
-  end
+      def index
+        @products = Product.all
+      end
 
 
-  def new
-    @product = Product.new
-  end
+      def new
+        @product = Product.new
+      end
 
-  def create
-    @product = Product.new
+      def create
+        @product = Product.new
 
-     @product.name = params[:product][:name]
-     @product.description = params[:product][:description]
-     @product.url = params[:product][:url]
-     @product.price_in_cents = params[:product][:price_in_cents]
-
-
-   if  @product.save
-       redirect_to :index
-        flash[:success] = "Product successful added!"
-     else
-       render :new
-   end
-  end
+         @product.name = params[:product][:name]
+         @product.description = params[:product][:description]
+         @product.url = params[:product][:url]
+         @product.price_in_cents = params[:product][:price_in_cents]
 
 
-  def show
-    @product        = Product.find(params[:id])
-  end
+       if  @product.save
+           redirect_to :index
+            flash[:success] = "Product successful added!"
+         else
+           render :new
+       end
+      end
+
+
+      def show
+        @product = Product.find(params[:id])
+      end
 
 
 
-  def edit
-    @product        = Product.find(params[:id])
-  end
+      def edit
+        @product = Product.find(params[:id])
+      end
 
-  def update
-  end
+      def update
+          @product = Product.find(params[:id])
 
-  def destroy
-  end
+
+         @product.name = params[:product][:name]
+         @product.description = params[:product][:description]
+         @product.url = params[:product][:url]
+         @product.price_in_cents = params[:product][:price_in_cents]
+
+         if  @product.save
+             redirect_to "/products/#{@product.id}"
+              flash[:success] = "Product successful updated!"
+           else
+             render :edit
+         end
+
+      end
+
+      def destroy
+         @product       = Product.find(params[:id])
+         @product.delete
+         redirect_to "/products"
+         flash[:success] = "Product successfully deleted!"
+
+      end
+
+
 
 
 end
