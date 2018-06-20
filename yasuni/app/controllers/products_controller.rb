8e0e908a9835411c6c5107aa016345a655aadca1
beewhoo <bibeksinghthapa@gmail.com>
2018-06-20 13:33:44 -0400

    added reviews_controller, created partial forms, fixed routes, created product controller methods and last but not least added URL onto product table at approximately June 20th 2018 1:33 and 15 seconds and 284018240  milliseconds!- bravo out!

--- yasuni/app/controllers/products_controller.rb
+++ yasuni/app/controllers/products_controller.rb
@@ -1,3 +1,47 @@
 class ProductsController < ApplicationController
-  
+
+  def index
+    @products = Product.all 
+  end
+
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new
+
+     @product.name = params[:product][:name]
+     @product.description = params[:product][:description]
+     @product.url = params[:product][:url]
+     @product.price_in_cents = params[:product][:price_in_cents]
+
+
+   if  @product.save
+       redirect_to :index
+        flash[:success] = "Product successful added!"
+     else
+       render :new
+   end
+  end
+
+
+  def show
+    @product        = Product.find(params[:id])
+  end
+
+
+
+  def edit
+    @product        = Product.find(params[:id])
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
 end
--- yasuni/db/schema.rb
+++ yasuni/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_20_152835) do
+ActiveRecord::Schema.define(version: 2018_06_20_164715) do
 
   create_table "products", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_06_20_152835) do
     t.string "name"
     t.text "description"
     t.integer "price_in_cents"
+    t.string "url"
   end
 
   create_table "reviews", force: :cascade do |t|
